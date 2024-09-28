5952904318db24a82596770411af33a64471adae
Luís Gomes <luis.gomes1990@outlook.com>
2024-09-28 12:32:57 +0100

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print("Phyton BootCamp Cubo3")
+
+#Variaveis
+Nome = "Luis"
+Idade = 110
+print(Idade)
+#Multipilcação
+NovaIdade = Idade * 10
+print(NovaIdade)
+#metodo
+Lista = [1,2,3,4,5]
+print(Lista)
+ListaInvertida = Lista.reverse
+print(ListaInvertida)
